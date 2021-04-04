988136a22f87c88bcfd64d467fb6e60c41fdbb2d
milonnayek <73352682+milonnayek@users.noreply.github.com>
2021-04-04 09:58:07 +0600

    Add files via upload
    fun time create like a priramid using javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+
+document.write("<center>")
+
+for(var x=0 ; x<=10 ; x++){
+    for( var y=0 ; y<=x ; y++){
+        document.write("** ")
+    }
+    document.write("</br>")
+};
+for(var i=1 ; i<=341 ; i = i + 1){
+    
+    document.write("=" + " ");
+}
